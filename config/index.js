78f0d66a9269c3b73f3736b7a1c5cf61827b09a2
lijch13 <50294785+lijch13@users.noreply.github.com>
2022-09-17 01:41:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -66,11 +66,11 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "nS-sTWb2p59qQU7Aw4dmp4t1x6tCYeYFTNq9JJ_c6C4",
       // 所在省份
-      province: "广东",
+      province: "贵州",
       // 所在城市
-      city: "肇庆",
+      city: "六盘水",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '09-12',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -78,7 +78,7 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+        {"type": "*生日", "name": "露露", "year": "1998", "date": "09-12"},
         //{"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         //{"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
